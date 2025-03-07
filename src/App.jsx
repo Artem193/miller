1c472dad568e8e9117e6ad8f62d01c356311b10c
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2025-03-07 18:07:06 +0200

    Deploy build

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.scss';
 
 function App() {
   return (
-    <Router basename='/miller-ins'>
+    <Router basename='./'>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/AboutUs' element={<AboutUs />}/>
