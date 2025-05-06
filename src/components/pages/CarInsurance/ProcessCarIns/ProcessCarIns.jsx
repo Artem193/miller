5d1e4820cc4251ac3c295bde40ecc65d5795351d
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2025-05-06 13:49:43 +0300

    demo

--- src/components/pages/CarInsurance/ProcessCarIns/ProcessCarIns.jsx
+++ src/components/pages/CarInsurance/ProcessCarIns/ProcessCarIns.jsx
@@ -25,6 +25,38 @@ export const ProcessCarIns = () => {
             <p className='processCarIns__text'>Оставьте заявку на сайте или свяжитесь с нами по телефону или WhatsApp</p>
           </div>
         </div>
+
+        <div className='processCarIns__item processCarIns__item--reverse'>
+          <h2 className='processCarIns__circle'>2
+          </h2>
+          <div className='processCarIns__box'>
+            <p className='processCarIns__text'>Получите консультацию и персональный расчёт стоимости — быстро, понятно, без навязывания</p>
+          </div>
+        </div>
+
+        <div className='processCarIns__item'>
+          <h2 className='processCarIns__circle'>3
+          </h2>
+          <div className='processCarIns__box'>
+            <p className='processCarIns__text'>Выберите подходящий вариант страхования с учётом вашего авто, бюджета и потребностей</p>
+          </div>
+        </div>
+
+        <div className='processCarIns__item processCarIns__item--reverse'>
+          <h2 className='processCarIns__circle'>4
+          </h2>
+          <div className='processCarIns__box'>
+            <p className='processCarIns__text'>Оформите полис удобно — полностью онлайн или с сопровождением персонального менеджера</p>
+          </div>
+        </div>
+
+        <div className='processCarIns__item'>
+          <h2 className='processCarIns__circle'>5
+          </h2>
+          <div className='processCarIns__box'>
+            <p className='processCarIns__text'>Получите все документы и поддержку — в случае ДТП мы рядом и поможем с урегулированием убытков</p>
+          </div>
+        </div>
       </div>
     </section>
   )
