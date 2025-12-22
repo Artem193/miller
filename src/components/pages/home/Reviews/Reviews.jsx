0ccf129a8fb319c6f94e03e15ecb987d020764c6
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2025-12-22 18:31:17 +0200

    add reviews

--- src/components/pages/home/Reviews/Reviews.jsx
+++ src/components/pages/home/Reviews/Reviews.jsx
@@ -36,12 +36,21 @@ export const Reviews = () => {
         }}
         className="reviews__swiper"
       >
-        {[1, 2, 3, 4].map((num) => (
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
           <SwiperSlide key={num}>
             <div className='reviews__block'>
               <div className='reviews__person'>
                 <h3 className='reviews__name'>{t(`home.reviews.name${num}`)}</h3>
                 <p className='reviews__city'>{t(`home.reviews.city${num}`)}</p>
+                <p className='reviews__date'>{t(`home.reviews.date${num}`)}</p>
+                <a
+                  href="https://www.bizreviews.co.il/provider/miller-anatoli"
+                  className="reviews__source"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {t(`home.reviews.source`)}
+                </a>
               </div>
               <p className='reviews__text'>{t(`home.reviews.text${num}`)}</p>
             </div>
